9bbf4d64bb2ff80525cf1ac43e31c58c09af3f4e
RyongJaeKwon <gyj343@gmail.com>
2024-07-04 17:03:53 +0900

    Fix: RefreshToken 기능 구현중

---
+++ src/main/java/com/kwon/myshop/controller/RefreshController.java
@@ -0,0 +1,15 @@
+package com.kwon.myshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class RefreshController {
+
+}
---
+++ src/main/java/com/kwon/myshop/domain/BaseEntity.java
@@ -0,0 +1,2 @@
+package com.kwon.myshop.domain;public class BaseEntity {
+}
