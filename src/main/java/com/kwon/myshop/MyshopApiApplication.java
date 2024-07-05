088111241dbc1508aa2a948095f921c3c9350e47
RyongJaeKwon <gyj343@gmail.com>
2024-07-05 19:02:35 +0900

    Fix: Auditing 기능 활성화

--- src/main/java/com/kwon/myshop/MyshopApiApplication.java
+++ src/main/java/com/kwon/myshop/MyshopApiApplication.java
@@ -2,8 +2,10 @@ package com.kwon.myshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyshopApiApplication {
 
     public static void main(String[] args) {
