07deeecd54850a9efbdef82a9c77e0e1c2d47326
RyongJaeKwon <gyj343@gmail.com>
2024-07-05 18:44:54 +0900

    Feat: ItemNotFoundException 예의 구현

--- src/main/java/com/kwon/myshop/exception/ItemNotFoundException.java
+++ src/main/java/com/kwon/myshop/exception/ItemNotFoundException.java
@@ -1,2 +1,17 @@
-package com.kwon.myshop.exception;public class ItemNotFoundException {
+package com.kwon.myshop.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ItemNotFoundException extends MyshopException {
+
+    private static final String MESSAGE = "존재하지 않는 상품입니다.";
+
+    public ItemNotFoundException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public HttpStatus getStatusCode() {
+        return HttpStatus.NOT_FOUND;
+    }
 }
