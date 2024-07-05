341059d85278bfda07564003e362ffcdc4583e3d
RyongJaeKwon <gyj343@gmail.com>
2024-07-05 18:48:11 +0900

    Fix: ItemRepository 기능 구현 중

--- src/main/java/com/kwon/myshop/repository/ItemRepository.java
+++ src/main/java/com/kwon/myshop/repository/ItemRepository.java
@@ -17,25 +17,19 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
      */
     @EntityGraph(attributePaths = "imageList")
     @Query("select i from Item i where i.id = :id and i.category = :category")
-    Optional<Item> selectOneWithCategoru(@Param("id") Long id);
+    Optional<Item> findWithCategory(@Param("id") Long id,
+                                         @Param("category") String category);
 
     /**
-     * 해당 카테고리의 아이템 목록 화면에서 아이템 하나당 대표이미지(ord = 0)만 가져오기
+     * 해당 카테고리의 아이템 목록 화면에서 가장 최근에 등록된 아이템의 대표이미지(ord = 0)만 페이징해서 가져오기
      */
-    @Query("select i, ii from Item i left join i.imageList ii where ii.ord = 0 and i.category = :category")
-    Page<Object[]> selectListWithCategory(Pageable pageable);
+    @Query("select i, ii from Item i left join i.imageList ii where ii.ord = 0 and i.category = :category order by i.regDate desc")
+    Page<Object[]> findRecentItemListWithCategory(Pageable pageable, @Param("category") String category);
 
     /**
-     * 아이템 엔티티와 연관된 imageList를 Eager로딩 한다
+     * 메인화면에서 모든 카테고리 포함 가장 최근에 등록된 아이템의 대표이미지(ord = 0)만 페이징해서 가져오기
      */
-    @EntityGraph(attributePaths = "imageList")
-    @Query("select i from Item i where i.id = :id")
-    Optional<Item> selectOne(@Param("id") Long id);
-
-    /**
-     * 가장 최근 등록된 아이템 목록(대표이미지(ord = 0)와 함께 조회
-     */
-    @Query("select i from Item i left join i.imageList ii where ii.ord = 0 order by i.regDate desc")
+    @Query("select i, ii from Item i left join i.imageList ii where ii.ord = 0 order by i.regDate desc")
     Page<Object[]> findRecentItemList(Pageable pageable);
 
 }
