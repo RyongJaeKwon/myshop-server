4d10b22575305c3decdf5211a6c60dcf18af6f62
RyongJaeKwon <gyj343@gmail.com>
2024-07-05 18:43:16 +0900

    Feat: 페이징 DTO 구현

--- src/main/java/com/kwon/myshop/dto/PageRequestDto.java
+++ src/main/java/com/kwon/myshop/dto/PageRequestDto.java
@@ -1,2 +1,16 @@
-package com.kwon.myshop.dto;public class PageRequestDto {
+package com.kwon.myshop.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Builder
+public class PageRequestDto {
+
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
 }
