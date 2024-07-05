8d8032ee8e0482c661a752aa84c9972302247579
RyongJaeKwon <gyj343@gmail.com>
2024-07-05 18:41:01 +0900

    Fix: 파일 업로드 필드 추가

--- src/main/java/com/kwon/myshop/dto/ItemDto.java
+++ src/main/java/com/kwon/myshop/dto/ItemDto.java
@@ -1,2 +1,27 @@
-package com.kwon.myshop.dto;public class ItemDto {
+package com.kwon.myshop.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+public class ItemDto {
+
+    private Long id;
+    private String itemName;
+    private String color;
+    private String size;
+    private String itemInfo;
+    private String brand;
+    private int price;
+    private String category;
+
+    @Builder.Default
+    private List<MultipartFile> files = new ArrayList<>();
+
+    @Builder.Default
+    private List<String> uploadFileNames = new ArrayList<>();
 }
