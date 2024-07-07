05ee7e2e4647514f0acd1db132daf37b505fd9f8
RyongJaeKwon <gyj343@gmail.com>
2024-07-07 23:43:45 +0900

    Fix: 페이징 응답 객체 구현

--- src/main/java/com/kwon/myshop/dto/PageResponseDto.java
+++ src/main/java/com/kwon/myshop/dto/PageResponseDto.java
@@ -1,11 +1,37 @@
 package com.kwon.myshop.dto;
 
+import com.kwon.myshop.domain.Item;
+import com.kwon.myshop.domain.ItemImage;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Getter
-@Builder
 public class PageResponseDto<E> {
 
+    private List<E> dtoList;
+    private PageRequestDto pageRequestDto;
+    private int totalPages, start, end, current;
+    private boolean prev, next;
+    private List<Integer> pageList;
 
+    @Builder
+    public PageResponseDto(List<E> dtoList, PageRequestDto pageRequestDto, int totalPages) {
+        this.dtoList = dtoList;
+        this.pageRequestDto = pageRequestDto;
+        this.totalPages = totalPages;
+        int tempEnd = (int)(Math.ceil(pageRequestDto.getPage() / 10.0)) * 10;
+        this.end = totalPages > tempEnd ? tempEnd : totalPages;
+        this.start = tempEnd - 9;
+        this.prev = start > 1;
+        this.next = totalPages > tempEnd;
+        this.current = pageRequestDto.getPage();
+        this.pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+
+    }
 }
