2bdee0af559640c84159b608f6395cb77c9c2223
RyongJaeKwon <gyj343@gmail.com>
2024-07-07 23:45:53 +0900

    Fix: ItemRepository 메서드 수정

--- src/main/java/com/kwon/myshop/repository/ItemRepository.java
+++ src/main/java/com/kwon/myshop/repository/ItemRepository.java
@@ -16,15 +16,14 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
      * 해당 카테고리의 아이템 엔티티와 연관된 imageList를 Eager로딩 한다
      */
     @EntityGraph(attributePaths = "imageList")
-    @Query("select i from Item i where i.id = :id and i.category = :category")
-    Optional<Item> findWithCategory(@Param("id") Long id,
-                                         @Param("category") String category);
+    @Query("select i from Item i where i.id = :id")
+    Optional<Item> findByIdWithImages(@Param("id") Long id);
 
     /**
      * 해당 카테고리의 아이템 목록 화면에서 가장 최근에 등록된 아이템의 대표이미지(ord = 0)만 페이징해서 가져오기
      */
     @Query("select i, ii from Item i left join i.imageList ii where ii.ord = 0 and i.category = :category order by i.regDate desc")
-    Page<Object[]> findRecentItemListWithCategory(Pageable pageable, @Param("category") String category);
+    Page<Object[]> findRecentItemListByCategory(Pageable pageable, @Param("category") String category);
 
     /**
      * 메인화면에서 모든 카테고리 포함 가장 최근에 등록된 아이템의 대표이미지(ord = 0)만 페이징해서 가져오기
