cf95700af4a8a23b392d7b4003d7336d416d4f71
RyongJaeKwon <gyj343@gmail.com>
2024-07-07 23:41:32 +0900

    Fix: regDate, modDate 추가

--- src/main/java/com/kwon/myshop/dto/ItemDto.java
+++ src/main/java/com/kwon/myshop/dto/ItemDto.java
@@ -2,12 +2,17 @@ package com.kwon.myshop.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
+import org.springframework.cglib.core.Local;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Builder
+@ToString
 public class ItemDto {
 
     private Long id;
@@ -18,6 +23,8 @@ public class ItemDto {
     private String brand;
     private int price;
     private String category;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
 
     @Builder.Default
     private List<MultipartFile> files = new ArrayList<>();
---
+++ src/test/java/com/kwon/myshop/service/ItemServiceTest.java
@@ -0,0 +1,2 @@
+package com.kwon.myshop.service;public class ItemServiceTest {
+}
