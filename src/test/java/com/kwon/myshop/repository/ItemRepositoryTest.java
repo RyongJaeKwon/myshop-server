90cc52127fea13d71b709cfca239dade95ee0936
RyongJaeKwon <gyj343@gmail.com>
2024-07-07 23:47:45 +0900

    Fix: 테스트 코드 수정

--- src/test/java/com/kwon/myshop/repository/ItemRepositoryTest.java
+++ src/test/java/com/kwon/myshop/repository/ItemRepositoryTest.java
@@ -11,13 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
 @SpringBootTest
-//@Transactional
 @Slf4j
 public class ItemRepositoryTest {
 
@@ -62,8 +61,11 @@ public class ItemRepositoryTest {
                     .brand(brand)
                     .build();
             item.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
+            item.addFileName("s_" + UUID.randomUUID().toString() + "_" + "aaa.jpg");
             item.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
+            item.addFileName("s_" + UUID.randomUUID().toString() + "_" + "bbb.jpg");
             item.addFileName(UUID.randomUUID().toString() + "_" + "ccc.jpg");
+            item.addFileName("s_" + UUID.randomUUID().toString() + "_" + "ccc.jpg");
 
             itemRepository.save(item);
         }
@@ -71,6 +73,7 @@ public class ItemRepositoryTest {
 
     @Test
     @DisplayName("아이템 등록")
+    @Transactional
     public void saveItemTest() throws Exception {
         //given
         Item item = Item.builder()
@@ -89,9 +92,11 @@ public class ItemRepositoryTest {
         Item createItem = itemRepository.save(item);
 
         //then
-        Item savedItem = itemRepository.findById(createItem.getId()).orElseThrow(() -> new ItemNotFoundException());
+        Item savedItem = itemRepository.findById(createItem.getId()).orElseThrow(ItemNotFoundException::new);
 
         Assertions.assertNotNull(savedItem);
+        Assertions.assertNotNull(savedItem.getImageList().get(0));
+        Assertions.assertNotNull(savedItem.getImageList().get(1));
         Assertions.assertEquals(createItem.getId(), savedItem.getId());
         Assertions.assertEquals(createItem.getItemName(), savedItem.getItemName());
         Assertions.assertEquals(createItem.getItemInfo(), savedItem.getItemInfo());
@@ -99,8 +104,6 @@ public class ItemRepositoryTest {
         Assertions.assertEquals(createItem.getColor(), savedItem.getColor());
         Assertions.assertEquals(createItem.getBrand(), savedItem.getBrand());
         Assertions.assertEquals(createItem.getCategory(), savedItem.getCategory());
-        Assertions.assertNotNull(savedItem.getImageList().get(0));
-        Assertions.assertNotNull(savedItem.getImageList().get(1));
         Assertions.assertEquals(createItem.getImageList().get(0).getFileName(), savedItem.getImageList().get(0).getFileName());
         Assertions.assertEquals(createItem.getImageList().get(1).getFileName(), savedItem.getImageList().get(1).getFileName());
         Assertions.assertEquals(createItem.getSize(), savedItem.getSize());
@@ -108,6 +111,7 @@ public class ItemRepositoryTest {
 
     @Test
     @DisplayName("아이템 모두조회")
+    @Transactional
     public void getItemListByIdTest() throws Exception {
         //given
         Item item1 = Item.builder().itemName("아이템1").itemInfo("아이템1 정보").price(10000).category("top").color("navy").size("small").brand("nike").build();
@@ -133,7 +137,10 @@ public class ItemRepositoryTest {
         Assertions.assertNotNull(itemList.get(2));
         Assertions.assertNotNull(itemList.get(3));
         Assertions.assertNotNull(itemList.get(4));
-        Assertions.assertNull(itemList.get(5));
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            itemList.get(5);
+        });
+        Assertions.assertNotNull(itemList.size() == 5);
         Assertions.assertEquals(createItem1.getId(), itemList.get(0).getId());
         Assertions.assertEquals(createItem2.getId(), itemList.get(1).getId());
         Assertions.assertEquals(createItem3.getId(), itemList.get(2).getId());
@@ -142,63 +149,79 @@ public class ItemRepositoryTest {
     }
 
     @Test
+    @DisplayName("아이템 조회 - 조회 실패 시 ItemNotFoundException 던지기")
+    @Transactional
+    public void findItemByIdNotFoundTest() {
+        //given
+        Long invalidItemId = 782L;
+
+        //expected
+        Assertions.assertThrows(ItemNotFoundException.class, () -> {
+            itemRepository.findById(invalidItemId).orElseThrow(ItemNotFoundException::new);
+        });
+    }
+
+    @Test
     @DisplayName("카테고리별 가장 최근에 등록된 아이템 조회")
+    @Transactional
     public void getItemListByCategoryTest() throws Exception {
         //given
-//        String[] categories = {"top", "bottom", "hat", "acc", "shoes", "outer", "bag"};
-//        String[] colors = {"black", "gray", "white", "navy"};
-//        String[] sizes = {"small", "medium", "large", "xlarge"};
-//        String[] brands = {"nike", "adidas", "new balance", "champion", "yale"};
-//        int[] prices = {10000, 15000, 20000, 25000, 30000};
-//
-//
-//        Random random = new Random();
-//        Map<String, Integer> itemCountMap = new HashMap<>();
-//        int MAX_ITEMS_PER_CATEGORY = 20;
-//
-//        for (int i = 0; i < 128; i++) {
-//            String category;
-//
-//            do {
-//                category = categories[random.nextInt(categories.length)];
-//            } while (itemCountMap.getOrDefault(category, 0) >= MAX_ITEMS_PER_CATEGORY);
-//
-//            itemCountMap.put(category, itemCountMap.getOrDefault(category, 0) + 1);
-//
-//            String color = colors[random.nextInt(colors.length)];
-//            String size = sizes[random.nextInt(sizes.length)];
-//            String brand = brands[random.nextInt(brands.length)];
-//            int price = prices[random.nextInt(prices.length)];
-//
-//            Item item = Item.builder()
-//                    .itemName("아이템" + i)
-//                    .itemInfo("아이템 정보" + i)
-//                    .price(price)
-//                    .category(category)
-//                    .color(color)
-//                    .size(size)
-//                    .brand(brand)
-//                    .build();
-//
-//            item.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
-//            item.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
-//
-//            itemRepository.save(item);
-//        }
-        Pageable pageable = PageRequest.of(1, 10, Sort.by("regDate").descending());
+        String[] categories = {"top", "bottom", "hat", "acc", "shoes", "outer", "bag"};
+        String[] colors = {"black", "gray", "white", "navy"};
+        String[] sizes = {"small", "medium", "large", "xlarge"};
+        String[] brands = {"nike", "adidas", "new balance", "champion", "yale"};
+        int[] prices = {10000, 15000, 20000, 25000, 30000};
+
+
+        Random random = new Random();
+        Map<String, Integer> itemCountMap = new HashMap<>();
+        int MAX_ITEMS_PER_CATEGORY = 20;
+
+        for (int i = 0; i < 128; i++) {
+            String category;
+
+            do {
+                category = categories[random.nextInt(categories.length)];
+            } while (itemCountMap.getOrDefault(category, 0) >= MAX_ITEMS_PER_CATEGORY);
+
+            itemCountMap.put(category, itemCountMap.getOrDefault(category, 0) + 1);
+
+            String color = colors[random.nextInt(colors.length)];
+            String size = sizes[random.nextInt(sizes.length)];
+            String brand = brands[random.nextInt(brands.length)];
+            int price = prices[random.nextInt(prices.length)];
+
+            Item item = Item.builder()
+                    .itemName("아이템" + i)
+                    .itemInfo("아이템 정보" + i)
+                    .price(price)
+                    .category(category)
+                    .color(color)
+                    .size(size)
+                    .brand(brand)
+                    .build();
+
+            item.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
+            item.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
+
+            itemRepository.save(item);
+        }
+
+        Pageable pageable = PageRequest.of(1, 10);
 
         //when
-        Page<Object[]> itemList = itemRepository.findRecentItemListWithCategory(pageable, "top");
+        Page<Object[]> itemList = itemRepository.findRecentItemListByCategory(pageable, "top");
         itemList.getContent().forEach(items -> log.info(Arrays.toString(items)));
 
-        List<Object[]> content = itemList.getContent();
+        List<Object[]> result = itemList.getContent();
 
         //then
         Assertions.assertFalse(itemList.isEmpty());
         Assertions.assertTrue(itemList.getSize() == 10);
 
+        // 내림차순 검증
         LocalDateTime previousRegDate = null;
-        for (Object[] objects : content) {
+        for (Object[] objects : result) {
             Item item = (Item) objects[0];
             if (previousRegDate != null) {
                 Assertions.assertTrue(item.getRegDate().isBefore(previousRegDate) || item.getRegDate().isEqual(previousRegDate));
@@ -210,62 +233,65 @@ public class ItemRepositoryTest {
 
     @Test
     @DisplayName("가장 최근에 등록된 아이템 조회")
+    @Transactional
     public void findRecentItemListTest() throws Exception {
         //given
-//        String[] categories = {"top", "bottom", "hat", "acc", "shoes", "outer", "bag"};
-//        String[] colors = {"black", "gray", "white", "navy"};
-//        String[] sizes = {"small", "medium", "large", "xlarge"};
-//        String[] brands = {"nike", "adidas", "new balance", "champion", "yale"};
-//        int[] prices = {10000, 15000, 20000, 25000, 30000};
-//
-//
-//        Random random = new Random();
-//        Map<String, Integer> itemCountMap = new HashMap<>();
-//        int MAX_ITEMS_PER_CATEGORY = 20;
-//
-//        for (int i = 0; i < 128; i++) {
-//            String category;
-//
-//            do {
-//                category = categories[random.nextInt(categories.length)];
-//            } while (itemCountMap.getOrDefault(category, 0) >= MAX_ITEMS_PER_CATEGORY);
-//
-//            itemCountMap.put(category, itemCountMap.getOrDefault(category, 0) + 1);
-//
-//            String color = colors[random.nextInt(colors.length)];
-//            String size = sizes[random.nextInt(sizes.length)];
-//            String brand = brands[random.nextInt(brands.length)];
-//            int price = prices[random.nextInt(prices.length)];
-//
-//            Item item = Item.builder()
-//                    .itemName("아이템" + i)
-//                    .itemInfo("아이템 정보" + i)
-//                    .price(price)
-//                    .category(category)
-//                    .color(color)
-//                    .size(size)
-//                    .brand(brand)
-//                    .build();
-//
-//            item.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
-//            item.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
-//
-//            itemRepository.save(item);
-//        }
-        Pageable pageable = PageRequest.of(0, 10, Sort.by("regDate").descending());
+        String[] categories = {"top", "bottom", "hat", "acc", "shoes", "outer", "bag"};
+        String[] colors = {"black", "gray", "white", "navy"};
+        String[] sizes = {"small", "medium", "large", "xlarge"};
+        String[] brands = {"nike", "adidas", "new balance", "champion", "yale"};
+        int[] prices = {10000, 15000, 20000, 25000, 30000};
+
+
+        Random random = new Random();
+        Map<String, Integer> itemCountMap = new HashMap<>();
+        int MAX_ITEMS_PER_CATEGORY = 20;
+
+        for (int i = 0; i < 128; i++) {
+            String category;
+
+            do {
+                category = categories[random.nextInt(categories.length)];
+            } while (itemCountMap.getOrDefault(category, 0) >= MAX_ITEMS_PER_CATEGORY);
+
+            itemCountMap.put(category, itemCountMap.getOrDefault(category, 0) + 1);
+
+            String color = colors[random.nextInt(colors.length)];
+            String size = sizes[random.nextInt(sizes.length)];
+            String brand = brands[random.nextInt(brands.length)];
+            int price = prices[random.nextInt(prices.length)];
+
+            Item item = Item.builder()
+                    .itemName("아이템" + i)
+                    .itemInfo("아이템 정보" + i)
+                    .price(price)
+                    .category(category)
+                    .color(color)
+                    .size(size)
+                    .brand(brand)
+                    .build();
+
+            item.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
+            item.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
+
+            itemRepository.save(item);
+        }
+
+        Pageable pageable = PageRequest.of(0, 10);
 
         //when
         Page<Object[]> itemList = itemRepository.findRecentItemList(pageable);
         itemList.getContent().forEach(items -> log.info(Arrays.toString(items)));
 
-        List<Object[]> content = itemList.getContent();
+        List<Object[]> result = itemList.getContent();
 
         //then
         Assertions.assertFalse(itemList.isEmpty());
         Assertions.assertTrue(itemList.getSize() == 10);
 
+        // 내림차순 검증
         LocalDateTime previousRegDate = null;
-        for (Object[] objects : content) {
+        for (Object[] objects : result) {
             Item item = (Item) objects[0];
             if (previousRegDate != null) {
                 Assertions.assertTrue(item.getRegDate().isBefore(previousRegDate) || item.getRegDate().isEqual(previousRegDate));
@@ -277,6 +303,7 @@ public class ItemRepositoryTest {
 
     @Test
     @DisplayName("아이템 수정")
+    @Transactional
     public void updateItemTest() throws Exception {
         //given
         Item item1 = Item.builder()
@@ -288,8 +315,23 @@ public class ItemRepositoryTest {
                 .size("small")
                 .brand("nike")
                 .build();
+        item1.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
+        item1.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
 
         Item createItem = itemRepository.save(item1);
+
+        // 객체 복제
+        Item originalItem = Item.builder()
+                .id(createItem.getId())
+                .itemName(createItem.getItemName())
+                .itemInfo(createItem.getItemInfo())
+                .price(createItem.getPrice())
+                .category(createItem.getCategory())
+                .color(createItem.getColor())
+                .size(createItem.getSize())
+                .brand(createItem.getBrand())
+                .imageList(new ArrayList<>(createItem.getImageList()))
+                .build();
 
         Item item2 = Item.builder()
                 .id(createItem.getId())
@@ -301,22 +343,25 @@ public class ItemRepositoryTest {
                 .size("large")
                 .brand("adidas")
                 .build();
+        item2.addFileName(UUID.randomUUID().toString() + "_" + "ccc.jpg");
 
         //when
         Item savedItem = itemRepository.save(item2);
 
         //then
-        Item updateItem = itemRepository.findById(savedItem.getId()).orElseThrow(() -> new ItemNotFoundException());
+        Item updateItem = itemRepository.findByIdWithImages(savedItem.getId()).orElseThrow(ItemNotFoundException::new);
 
         Assertions.assertNotNull(updateItem);
         Assertions.assertEquals(createItem.getId(), updateItem.getId());
-        Assertions.assertNotEquals(createItem.getItemName(), updateItem.getItemName());
-        Assertions.assertNotEquals(createItem.getItemInfo(), updateItem.getItemInfo());
-        Assertions.assertNotEquals(createItem.getPrice(), updateItem.getPrice());
-        Assertions.assertNotEquals(createItem.getColor(), updateItem.getColor());
-        Assertions.assertNotEquals(createItem.getBrand(), updateItem.getBrand());
-        Assertions.assertNotEquals(createItem.getCategory(), updateItem.getCategory());
-        Assertions.assertNotEquals(createItem.getSize(), updateItem.getSize());
+        Assertions.assertNotEquals(originalItem.getItemName(), updateItem.getItemName());
+        Assertions.assertNotEquals(originalItem.getItemInfo(), updateItem.getItemInfo());
+        Assertions.assertNotEquals(originalItem.getPrice(), updateItem.getPrice());
+        Assertions.assertNotEquals(originalItem.getColor(), updateItem.getColor());
+        Assertions.assertNotEquals(originalItem.getBrand(), updateItem.getBrand());
+        Assertions.assertNotEquals(originalItem.getCategory(), updateItem.getCategory());
+        Assertions.assertNotEquals(originalItem.getSize(), updateItem.getSize());
+        // 이미지 리스트 검증
+        Assertions.assertEquals(1, updateItem.getImageList().size());
     }
 
 }
