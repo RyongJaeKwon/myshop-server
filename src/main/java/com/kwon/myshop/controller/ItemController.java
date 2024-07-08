77a6a4db3f6d07259276939fe10cd0e12990de4a
RyongJaeKwon <gyj343@gmail.com>
2024-07-08 13:14:52 +0900

    Feat: ItemController 구현

--- src/main/java/com/kwon/myshop/controller/ItemController.java
+++ src/main/java/com/kwon/myshop/controller/ItemController.java
@@ -1,7 +1,10 @@
 package com.kwon.myshop.controller;
 
 import com.kwon.myshop.dto.ItemDto;
+import com.kwon.myshop.dto.PageRequestDto;
+import com.kwon.myshop.dto.PageResponseDto;
 import com.kwon.myshop.file.FileUtil;
+import com.kwon.myshop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
@@ -11,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,25 +23,86 @@ import java.util.Map;
 public class ItemController {
 
     private final FileUtil fileUtil;
+    private final ItemService itemService;
 
     @PostMapping("/")
-    public Map<String, String> create(ItemDto itemDto) {
-
+    public Map<String, Long> create(@RequestBody ItemDto itemDto) {
         List<MultipartFile> files = itemDto.getFiles();
         List<String> uploadFileNames = fileUtil.addFiles(files);
 
-        ItemDto savedItemDto = ItemDto.builder()
-                .uploadFileNames(uploadFileNames)
+        ItemDto createItemDto = ItemDto.builder()
+                .itemName(itemDto.getItemName())
+                .color(itemDto.getColor())
+                .size(itemDto.getSize())
+                .itemInfo(itemDto.getItemInfo())
+                .brand(itemDto.getBrand())
+                .price(itemDto.getPrice())
+                .category(itemDto.getCategory())
+                .uploadFileNames(uploadFileNames.size() > 0 ? uploadFileNames : itemDto.getUploadFileNames())
                 .build();
 
-        log.info(uploadFileNames.get(0));
+        log.info("createItemDto : " + createItemDto);
+        Long id = itemService.create(createItemDto);
 
-        return Map.of("RESULT", "SUCCESS");
+        return Map.of("RESULT", id);
+    }
+
+    @GetMapping("/")
+    public PageResponseDto<ItemDto> list(PageRequestDto pageRequestDto) {
+        return itemService.getRecentItems(pageRequestDto);
+    }
+
+    @GetMapping("/{category}")
+    public PageResponseDto<ItemDto> categoryList(PageRequestDto pageRequestDto, @PathVariable String category) {
+        return itemService.getRecentItemsWithCategory(pageRequestDto, category);
+    }
+
+    @GetMapping("/{id}")
+    public ItemDto read(@PathVariable Long id) {
+        return itemService.getItemWithImages(id);
+    }
+
+    @PutMapping("/")
+    public Map<String, String> update(@RequestBody ItemDto itemDto) {
+        try {
+            ItemDto savedItemDto = itemService.getItemWithImages(itemDto.getId());
+            List<String> savedUploadFileNames = savedItemDto.getUploadFileNames();
+
+            List<MultipartFile> files = itemDto.getFiles();
+            List<String> recentUploadFileNames = fileUtil.addFiles(files);
+            List<String> uploadFileNames = itemDto.getUploadFileNames();
+
+            if (recentUploadFileNames != null && !recentUploadFileNames.isEmpty()) {
+                uploadFileNames.addAll(recentUploadFileNames);
+            }
+
+            itemService.update(itemDto);
+
+            if (savedUploadFileNames != null && !savedUploadFileNames.isEmpty()) {
+                List<String> deleteFiles = savedUploadFileNames.stream()
+                        .filter(fileName -> !uploadFileNames.contains(fileName))
+                        .collect(Collectors.toList());
+
+                fileUtil.deleteFiles(deleteFiles);
+            }
+
+            return Map.of("RESULT", "SUCCESS");
+        } catch (Exception e) {
+            return Map.of("RESULT", "FAILURE", "MESSAGE", e.getMessage());
+        }
     }
 
     @GetMapping("/view/{fileName}")
-    public ResponseEntity<Resource> GetOneFile(@PathVariable String fileName) {
+    public ResponseEntity<Resource> viewFile(@PathVariable String fileName) {
         return fileUtil.getFile(fileName);
+    }
+
+    @DeleteMapping("/{id}")
+    public Map<String, String> delete(@PathVariable Long id) {
+        List<String> savedUploadFileNames = itemService.getItemWithImages(id).getUploadFileNames();
+        itemService.delete(id);
+        fileUtil.deleteFiles(savedUploadFileNames);
+        return Map.of("RESULT", "SUCCESS");
     }
 
 }
---
+++ src/test/java/com/kwon/myshop/controller/ItemControllerTest.java
@@ -0,0 +1,89 @@
+package com.kwon.myshop.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kwon.myshop.domain.Member;
+import com.kwon.myshop.dto.AddressDto;
+import com.kwon.myshop.dto.ItemDto;
+import com.kwon.myshop.dto.MemberDto;
+import com.kwon.myshop.repository.ItemRepository;
+import com.kwon.myshop.service.ItemService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ItemController {
+    
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private ItemService itemService;
+    
+    @Autowired
+    private ItemRepository itemRepository;
+    
+    @Autowired
+    private ObjectMapper objectMapper;
+    
+    @Test
+    @DisplayName("회원가입")
+    @Transactional
+    public void createTest() throws Exception {
+        //given
+        ItemDto createItemDto = ItemDto.builder()
+                .itemName("아이템이요")
+                .color("초록색")
+                .size("large")
+                .itemInfo("긴팔")
+                .brand("nike")
+                .price(27000)
+                .category("top")
+                .uploadFileNames(List.of(UUID.randomUUID().toString() + "_" + "Test1.jpg",
+                        UUID.randomUUID().toString() + "_" + "Test2.jpg"))
+                .build();
+
+        String json = objectMapper.writeValueAsString(createItemDto);
+
+        //expected
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/items/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+        Map<String, Long> map = objectMapper.readValue(content, new TypeReference<Map<String, Long>>() {});
+        Long createItemId = map.get("RESULT");
+
+        ItemDto savedItemDto = itemService.getItemWithImages(createItemId);
+        Assertions.assertNotNull(savedItemDto);
+        Assertions.assertEquals(createItemDto.getItemName(), savedItemDto.getItemName());
+        Assertions.assertEquals(createItemDto.getColor(), savedItemDto.getColor());
+        Assertions.assertEquals(createItemDto.getSize(), savedItemDto.getSize());
+        Assertions.assertEquals(createItemDto.getItemInfo(), savedItemDto.getItemInfo());
+        Assertions.assertEquals(createItemDto.getBrand(), savedItemDto.getBrand());
+        Assertions.assertEquals(createItemDto.getPrice(), savedItemDto.getPrice());
+        Assertions.assertEquals(createItemDto.getCategory(), savedItemDto.getCategory());
+        Assertions.assertTrue(savedItemDto.getUploadFileNames().size() == 2);
+
+    }
+
+}
