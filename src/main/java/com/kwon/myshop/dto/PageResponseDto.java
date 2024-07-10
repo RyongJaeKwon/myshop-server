edaa9406d1e020bb0bf9deef37a052f82714c0fa
RyongJaeKwon <gyj343@gmail.com>
2024-07-10 16:38:08 +0900

    Fix: PageResponseDto 로직 수정

--- src/main/java/com/kwon/myshop/dto/PageResponseDto.java
+++ src/main/java/com/kwon/myshop/dto/PageResponseDto.java
@@ -16,22 +16,35 @@ public class PageResponseDto<E> {
 
     private List<E> dtoList;
     private PageRequestDto pageRequestDto;
-    private int totalPages, start, end, current;
+    private int totalPages, start, end, current, prevPage, nextPage;
     private boolean prev, next;
     private List<Integer> pageList;
 
     @Builder
     public PageResponseDto(List<E> dtoList, PageRequestDto pageRequestDto, int totalPages) {
         this.dtoList = dtoList;
+        this.current = pageRequestDto.getPage();
         this.pageRequestDto = pageRequestDto;
         this.totalPages = totalPages;
         int tempEnd = (int)(Math.ceil(pageRequestDto.getPage() / 10.0)) * 10;
         this.end = totalPages > tempEnd ? tempEnd : totalPages;
         this.start = tempEnd - 9;
-        this.prev = start > 1;
-        this.next = totalPages > tempEnd;
-        this.current = pageRequestDto.getPage();
+        this.prev = current > 1;
+        this.next = current < totalPages;
         this.pageList = IntStream.rangeClosed(start, end).boxed().collect(Collectors.toList());
+        this.prevPage = getPrevPage();
+        this.nextPage = getNextPage();
 
+        System.out.println("Current: " + current);
+        System.out.println("Prev: " + prev);
+        System.out.println("Next: " + next);
+    }
+
+    public int getPrevPage() {
+        return current > 1 ? current - 1 : 1;
+    }
+
+    public int getNextPage() {
+        return current < totalPages ? current + 1 : totalPages;
     }
 }
