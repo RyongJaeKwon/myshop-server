4bd6be6d7d422ef139e7aae7999c22e46b475bdb
RyongJaeKwon <gyj343@gmail.com>
2024-07-15 23:27:20 +0900

    Feat: CustomAccessDeniedHandler 구현

--- src/main/java/com/kwon/myshop/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/kwon/myshop/security/CustomAccessDeniedHandler.java
@@ -1,2 +1,28 @@
-package com.kwon.myshop.security;public class CustomAccessDeniedHandler {
+package com.kwon.myshop.security;
+
+import com.google.gson.Gson;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(Map.of("error", "ERROR_ACCESSDENIED"));
+
+        response.setContentType("application/json; charset=UTF-8");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+
+        response.getWriter().write(json);
+    }
 }
