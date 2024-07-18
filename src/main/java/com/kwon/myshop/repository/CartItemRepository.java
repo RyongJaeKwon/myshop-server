360f0b6d6adcbb54128b745dc049186e0c3262c0
RyongJaeKwon <gyj343@gmail.com>
2024-07-19 04:23:02 +0900

    Feat: 장바구니 아이템 Repository 구현

--- src/main/java/com/kwon/myshop/repository/CartItemRepository.java
+++ src/main/java/com/kwon/myshop/repository/CartItemRepository.java
@@ -1,2 +1,51 @@
-package com.kwon.myshop.repository;public interface CartItemRepository {
+package com.kwon.myshop.repository;
+
+import com.kwon.myshop.domain.CartItem;
+import com.kwon.myshop.dto.CartItemListDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query("select " +
+            "new com.kwon.myshop.dto.CartItemListDto(ci.id, i.id, i.itemName, i.price, ci.quantity, ii.fileName) " +
+            "from " +
+            "CartItem ci inner join Cart c on ci.cart = c " +
+            "left join Item i on ci.item = i " +
+            "left join i.imageList ii " +
+            "where " +
+            "c.member.userId = :userId and ii.ord = 0 " +
+            "order by ci.regDate desc ")
+    List<CartItemListDto> getCartItemListByUserId(@Param("userId") String userId);
+
+    @Query("select " +
+            "ci " +
+            "from " +
+            "CartItem ci inner join Cart c on ci.cart = c " +
+            "where " +
+            "c.member.userId = :userId and ci.item.id = :itemId")
+    CartItem getCartItemByItemId(@Param("userId") String userId,
+                                 @Param("itemId") Long itemId);
+
+    @Query("select " +
+            "c.id " +
+            "from " +
+            "Cart c inner join CartItem ci on ci.cart = c " +
+            "where " +
+            "ci.id = :cartItemId")
+    Long getCartByCartItemId(@Param("cartItemId") Long cartItemId);
+
+    @Query("select " +
+            "new com.kwon.myshop.dto.CartItemListDto(ci.id, i.id, i.itemName, i.price, ci.quantity, ii.fileName) " +
+            "from " +
+            "CartItem ci inner join Cart c on ci.cart = c " +
+            "left join Item i on ci.item = i " +
+            "left join i.imageList ii " +
+            "where " +
+            "c.id = :id and ii.ord = 0 " +
+            "order by ci.regDate desc ")
+    List<CartItemListDto> getCartItemListDtoByCart(@Param("id") Long id);
 }
