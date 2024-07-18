ae214144bfc1aeb6e2c7481006733c3f1a0e2ce0
RyongJaeKwon <gyj343@gmail.com>
2024-07-19 04:19:23 +0900

    Feat: 장바구니 아이템 등록, 수정을 위한 Dto 추가

--- src/main/java/com/kwon/myshop/dto/CartItemDto.java
+++ src/main/java/com/kwon/myshop/dto/CartItemDto.java
@@ -1,2 +1,14 @@
-package com.kwon.myshop.dto;public class CartItemDto {
+package com.kwon.myshop.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CartItemDto {
+
+    private String userId;
+    private Long cartItemId;
+    private Long itemId;
+    private int quantity;
 }
