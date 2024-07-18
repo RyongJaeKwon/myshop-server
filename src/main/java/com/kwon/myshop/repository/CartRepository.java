b74c62182c08c81bda3947cbdc5fa811a7c96d2a
RyongJaeKwon <gyj343@gmail.com>
2024-07-19 04:23:57 +0900

    Feat: 장바구니 Repository 구현

--- src/main/java/com/kwon/myshop/repository/CartRepository.java
+++ src/main/java/com/kwon/myshop/repository/CartRepository.java
@@ -1,2 +1,14 @@
-package com.kwon.myshop.repository;public interface CartRepository {
+package com.kwon.myshop.repository;
+
+import com.kwon.myshop.domain.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    @Query("select c from Cart c where c.member.userId = :userId")
+    Optional<Cart> getCartByMember(@Param("userId") String userId);
 }
