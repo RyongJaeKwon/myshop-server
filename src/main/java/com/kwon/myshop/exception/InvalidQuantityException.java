004dd6501059bcc782240af697e337214fde55e2
RyongJaeKwon <gyj343@gmail.com>
2024-07-20 05:04:38 +0900

    Feat: 수량 유효성 검사 예외 구현

--- src/main/java/com/kwon/myshop/exception/InvalidQuantityException.java
+++ src/main/java/com/kwon/myshop/exception/InvalidQuantityException.java
@@ -1,2 +1,17 @@
-package com.kwon.myshop.exception;public class InvalidQuantityException {
+package com.kwon.myshop.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidQuantityException extends MyshopException {
+
+    private static final String MESSAGE = "더 이상 수량을 줄일 수 없습니다";
+
+    public InvalidQuantityException() {
+        super(MESSAGE);
+    }
+
+    @Override
+    public HttpStatus getStatusCode() {
+        return HttpStatus.BAD_REQUEST;
+    }
 }
