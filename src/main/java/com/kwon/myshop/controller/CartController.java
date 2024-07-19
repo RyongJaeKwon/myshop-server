28e55bf1e3cd010038051ffe20f4c24e17a6773f
RyongJaeKwon <gyj343@gmail.com>
2024-07-20 05:08:12 +0900

    Fix: 컨트롤러에 인증된 사용자 권한 확인 추가

--- src/main/java/com/kwon/myshop/controller/CartController.java
+++ src/main/java/com/kwon/myshop/controller/CartController.java
@@ -1,2 +1,41 @@
-package com.kwon.myshop.controller;public class CartController {
+package com.kwon.myshop.controller;
+
+import com.kwon.myshop.dto.CartItemDto;
+import com.kwon.myshop.dto.CartItemListDto;
+import com.kwon.myshop.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    @PreAuthorize("#cartItemDto.userId == authentication.name")
+    @PostMapping("/")
+    public List<CartItemListDto> changeCart(@RequestBody CartItemDto cartItemDto) {
+
+        return cartService.create(cartItemDto);
+    }
+
+    @PreAuthorize("#principal.name == authentication.name")
+    @GetMapping("/list")
+    public List<CartItemListDto> list(Principal principal) {
+        String userId = principal.getName();
+
+        return cartService.getCartItems(userId);
+    }
+
+    @PreAuthorize("#principal.name == authentication.name")
+    @DeleteMapping("/{cartItemId}")
+    public List<CartItemListDto> delete(@PathVariable Long cartItemId, Principal principal) {
+
+        return cartService.delete(cartItemId);
+    }
 }
