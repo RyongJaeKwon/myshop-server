6e1f65b4c1712017a1ac24c4824d7b0f789bd665
RyongJaeKwon <gyj343@gmail.com>
2024-07-25 03:01:19 +0900

    Feat: 댓글 DTO 클래스 추가

--- src/main/java/com/kwon/myshop/dto/ReplyDto.java
+++ src/main/java/com/kwon/myshop/dto/ReplyDto.java
@@ -1,2 +1,20 @@
-package com.kwon.myshop.dto;public class ReplyDto {
+package com.kwon.myshop.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@ToString
+public class ReplyDto {
+
+    private Long id;
+    private Long memberId;
+    private Long itemId;
+    private String content;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
 }
