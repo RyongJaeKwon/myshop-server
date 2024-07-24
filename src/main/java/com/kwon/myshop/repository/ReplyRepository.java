6760a12bc5970bb9ded1c2ba9492a6894c026f80
RyongJaeKwon <gyj343@gmail.com>
2024-07-25 02:59:08 +0900

    Feat: ReplyRepository 구현

--- src/main/java/com/kwon/myshop/repository/ReplyRepository.java
+++ src/main/java/com/kwon/myshop/repository/ReplyRepository.java
@@ -1,2 +1,11 @@
-package com.kwon.myshop.repository;public interface ReplyRepository {
+package com.kwon.myshop.repository;
+
+import com.kwon.myshop.domain.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findByItemId(Long itemId);
 }
