990722d0de645e06d7abcfcc743c1ecd8b816b02
RyongJaeKwon <gyj343@gmail.com>
2024-07-25 02:59:54 +0900

    Feat: 존재하지 않는 댓글에 대한 예외 처리 구현

--- src/main/java/com/kwon/myshop/exception/ReplyNotFoundException.java
+++ src/main/java/com/kwon/myshop/exception/ReplyNotFoundException.java
@@ -1,2 +1,16 @@
-package com.kwon.myshop.exception;public class ReplyNotFoundException {
+package com.kwon.myshop.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ReplyNotFoundException extends MyshopException{
+
+    private static final String MESSAGE = "존재하지 않는 댓글입니다.";
+
+    public ReplyNotFoundException() {
+        super(MESSAGE);
+    }
+    @Override
+    public HttpStatus getStatusCode() {
+        return HttpStatus.NOT_FOUND;
+    }
 }
