dedb25d5eb669dc84f4fbc441f048c3891bc313d
RyongJaeKwon <gyj343@gmail.com>
2024-07-26 05:30:23 +0900

    Feat: Projection을 사용하여 상품별 리뷰 목록 조회 기능 추가

--- src/main/java/com/kwon/myshop/repository/ReplyRepository.java
+++ src/main/java/com/kwon/myshop/repository/ReplyRepository.java
@@ -1,11 +1,24 @@
 package com.kwon.myshop.repository;
 
 import com.kwon.myshop.domain.Reply;
+import com.kwon.myshop.dto.CartItemListDto;
+import com.kwon.myshop.dto.ReplyDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
-    List<Reply> findByItemId(Long itemId);
+    @Query("select " +
+            "new com.kwon.myshop.dto.ReplyDto(r.id, m.id, i.id, m.userId, r.content, r.regDate, r.modDate) " +
+            "from " +
+            "Reply r left join r.item i " +
+            "left join r.member m " +
+            "where " +
+            "i.id = :itemId " +
+            "order by r.regDate DESC")
+    List<ReplyDto> getReplyListByItemId(@Param("itemId") Long itemId);
+
 }
