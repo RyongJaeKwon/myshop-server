4975ae5b197ac3b5bf87f14631ffbf832fc5e0c8
RyongJaeKwon <gyj343@gmail.com>
2024-07-26 05:25:20 +0900

    Feat: userId 필드 추가

--- src/main/java/com/kwon/myshop/dto/ReplyDto.java
+++ src/main/java/com/kwon/myshop/dto/ReplyDto.java
@@ -1,5 +1,6 @@
 package com.kwon.myshop.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -9,11 +10,13 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @ToString
+@AllArgsConstructor
 public class ReplyDto {
 
     private Long id;
     private Long memberId;
     private Long itemId;
+    private String userId;
     private String content;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
