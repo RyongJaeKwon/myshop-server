9b1ae23a2ae25c5099a7c6f8dd4f5b2da7c2dcf4
RyongJaeKwon <gyj343@gmail.com>
2024-07-31 04:03:22 +0900

    Feat: @ToString 추가

--- src/main/java/com/kwon/myshop/dto/OrderItemDto.java
+++ src/main/java/com/kwon/myshop/dto/OrderItemDto.java
@@ -3,10 +3,12 @@ package com.kwon.myshop.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@ToString
 public class OrderItemDto {
 
     private Long itemId;
