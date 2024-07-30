d3f401f3f695dfc7ce6a1db0b20db3f59a666722
RyongJaeKwon <gyj343@gmail.com>
2024-07-31 04:10:16 +0900

    Feat: OrderService에 대한 주요 기능 테스트 추가

---
+++ src/test/java/com/kwon/myshop/service/OrderServiceTest.java
@@ -0,0 +1,330 @@
+package com.kwon.myshop.service;
+
+import com.kwon.myshop.domain.*;
+import com.kwon.myshop.dto.OrderCreateDto;
+import com.kwon.myshop.dto.OrderDto;
+import com.kwon.myshop.dto.OrderItemDto;
+import com.kwon.myshop.repository.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@SpringBootTest
+@Transactional
+@Slf4j
+public class OrderServiceTest {
+
+    @Autowired private OrderService orderService;
+    @Autowired private OrderRepository orderRepository;
+    @Autowired private OrderItemRepository orderItemRepository;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private ItemRepository itemRepository;
+    @Autowired private CartRepository cartRepository;
+    @Autowired private CartItemRepository cartItemRepository;
+
+    private Member createMember(String userId) {
+        return memberRepository.save(Member.builder()
+                .userId(userId)
+                .password("password")
+                .email("test@example.com")
+                .name("Test User")
+                .phone("010-4541-8783")
+                .role(Role.MEMBER)
+                .address(Address.builder()
+                        .postcode("12345")
+                        .basic_address("금정구")
+                        .detail_address("금정산성").build())
+                .build());
+    }
+
+    private Item createItem(String itemName, String itemInfo, int price, String category, String color, String size, String brand) {
+        Item item = Item.builder()
+                .itemName(itemName)
+                .itemInfo(itemInfo)
+                .price(price)
+                .category(category)
+                .color(color)
+                .size(size)
+                .brand(brand)
+                .build();
+        item.addFileName(UUID.randomUUID().toString() + "_" + "aaa.jpg");
+        item.addFileName(UUID.randomUUID().toString() + "_" + "bbb.jpg");
+
+        return itemRepository.save(item);
+    }
+
+    private Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = Order.builder()
+                .member(member)
+                .orderItems(List.of(orderItems))
+                .delivery(delivery)
+                .status(OrderStatus.ORDER)
+                .orderDate(LocalDateTime.now())
+                .totalPrice(orderItems[0].getOrderPrice() * orderItems[0].getQuantity() +
+                        orderItems[1].getOrderPrice() * orderItems[1].getQuantity())
+                .build();
+
+        member.setOrder(order);
+
+        return orderRepository.save(order);
+    }
+
+    @Test
+    @DisplayName("주문 생성")
+    @Transactional
+    public void createOrderTest() throws Exception {
+        //given
+        Member member = createMember("user1");
+
+        Item item1 = createItem("Item1", "Item1 info", 25000, "hat", "black", "large", "mlb");
+
+        OrderCreateDto orderCreateDto = OrderCreateDto.builder()
+                .userId("user1")
+                .orderItems(Arrays.asList(
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item1.getId())
+                                .price(item1.getPrice())
+                                .quantity(2)
+                                .build()))
+                .postcode("12345")
+                .basicAddress("Basic Address")
+                .detailAddress("Detail Address")
+                .receiverName("John Doe")
+                .receiverPhone("123-456-7890")
+                .message("Message")
+                .build();
+
+        //when
+        orderService.createOrder(orderCreateDto);
+
+        //then
+        List<Order> savedOrders = orderRepository.findOrdersByUserId(member.getUserId());
+
+        Assertions.assertNotNull(savedOrders);
+        Assertions.assertEquals(50000, savedOrders.get(0).getTotalPrice());
+        Assertions.assertEquals(OrderStatus.ORDER, savedOrders.get(0).getStatus());
+    }
+
+    @Test
+    @DisplayName("장바구니에서 주문 생성")
+    @Transactional
+    public void createOrderFromCartTest() throws Exception {
+        //given
+        Member member = createMember("user1");
+
+        Item item1 = createItem("Item1", "Item1 info", 25000, "hat", "black", "large", "mlb");
+        Item item2 = createItem("Item2", "Item2 info", 20000, "top", "white", "medium", "nike");
+
+        Cart cart = cartRepository.save(Cart.builder().member(member).build());
+
+        CartItem cartItem1 = CartItem.builder()
+                .item(item1)
+                .cart(cart)
+                .quantity(2)
+                .build();
+        cartItemRepository.save(cartItem1);
+
+        CartItem cartItem2 = CartItem.builder()
+                .item(item2)
+                .cart(cart)
+                .quantity(1)
+                .build();
+        cartItemRepository.save(cartItem2);
+
+        OrderCreateDto orderCreateDto = OrderCreateDto.builder()
+                .userId("user1")
+                .postcode("12345")
+                .basicAddress("Basic Address")
+                .detailAddress("Detail Address")
+                .receiverName("John Doe")
+                .receiverPhone("123-456-7890")
+                .message("Message")
+                .build();
+
+        //when
+        orderService.createOrderFromCart(orderCreateDto);
+
+        //then
+        List<Order> savedOrders = orderRepository.findOrdersByUserId(member.getUserId());
+
+        Assertions.assertNotNull(savedOrders);
+        Assertions.assertEquals(70000, savedOrders.get(0).getTotalPrice());
+        Assertions.assertEquals(OrderStatus.ORDER, savedOrders.get(0).getStatus());
+        Assertions.assertEquals(2, savedOrders.get(0).getOrderItems().size());
+    }
+
+    @Test
+    @DisplayName("userId로 주문 검색")
+    @Transactional
+    public void getOrdersByUserIdTest() throws Exception {
+        //given
+        Member member = createMember("user1");
+
+        Item item1 = createItem("Item1", "Item1 info", 25000, "hat", "black", "large", "mlb");
+        Item item2 = createItem("Item2", "Item2 info", 20000, "top", "white", "medium", "nike");
+        Item item3 = createItem("Item3", "Item3 info", 23000, "bottom", "blue", "small", "mood");
+
+        OrderCreateDto orderCreateDto1 = OrderCreateDto.builder()
+                .userId("user1")
+                .orderItems(Arrays.asList(
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item1.getId())
+                                .price(item1.getPrice())
+                                .quantity(1)
+                                .build(),
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item2.getId())
+                                .price(item2.getPrice())
+                                .quantity(2)
+                                .build(),
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item3.getId())
+                                .price(item3.getPrice())
+                                .quantity(2)
+                                .build()))
+                .postcode("12345")
+                .basicAddress("Basic Address")
+                .detailAddress("Detail Address")
+                .receiverName("John Doe")
+                .receiverPhone("123-456-7890")
+                .message("Message")
+                .build();
+
+        orderService.createOrder(orderCreateDto1);
+
+        Cart cart = cartRepository.save(Cart.builder().member(member).build());
+
+        CartItem cartItem1 = CartItem.builder()
+                .item(item3)
+                .cart(cart)
+                .quantity(2)
+                .build();
+        cartItemRepository.save(cartItem1);
+
+        CartItem cartItem2 = CartItem.builder()
+                .item(item2)
+                .cart(cart)
+                .quantity(1)
+                .build();
+        cartItemRepository.save(cartItem2);
+
+        OrderCreateDto orderCreateDto2 = OrderCreateDto.builder()
+                .userId("user1")
+                .postcode("12345")
+                .basicAddress("Basic Address")
+                .detailAddress("Detail Address")
+                .receiverName("John Doe")
+                .receiverPhone("123-456-7890")
+                .message("Message")
+                .build();
+
+        orderService.createOrderFromCart(orderCreateDto2);
+
+        //when
+        List<OrderDto> savedOrders = orderService.getOrdersByUserId(member.getUserId());
+
+        //then
+
+        Assertions.assertNotNull(savedOrders);
+        Assertions.assertEquals(66000, savedOrders.get(0).getTotalPrice());
+        Assertions.assertEquals(111000, savedOrders.get(1).getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("orderId로 주문 상세 검색")
+    @Transactional
+    public void getOrderItemsByUserIdTest() throws Exception {
+        //given
+        Member member = createMember("user1");
+
+        Item item1 = createItem("Item1", "Item1 info", 25000, "hat", "black", "large", "mlb");
+        Item item2 = createItem("Item2", "Item2 info", 20000, "top", "white", "medium", "nike");
+        Item item3 = createItem("Item3", "Item3 info", 23000, "bottom", "blue", "small", "mood");
+
+        OrderCreateDto orderCreateDto1 = OrderCreateDto.builder()
+                .userId("user1")
+                .orderItems(Arrays.asList(
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item1.getId())
+                                .price(item1.getPrice())
+                                .quantity(1)
+                                .build(),
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item2.getId())
+                                .price(item2.getPrice())
+                                .quantity(2)
+                                .build(),
+                        OrderCreateDto.OrderItemDto.builder()
+                                .itemId(item3.getId())
+                                .price(item3.getPrice())
+                                .quantity(2)
+                                .build()))
+                .postcode("12345")
+                .basicAddress("Basic Address")
+                .detailAddress("Detail Address")
+                .receiverName("John Doe")
+                .receiverPhone("123-456-7890")
+                .message("Message")
+                .build();
+
+        Long orderId1 = orderService.createOrder(orderCreateDto1);
+
+        Cart cart = cartRepository.save(Cart.builder().member(member).build());
+
+        CartItem cartItem1 = CartItem.builder()
+                .item(item3)
+                .cart(cart)
+                .quantity(2)
+                .build();
+        cartItemRepository.save(cartItem1);
+
+        CartItem cartItem2 = CartItem.builder()
+                .item(item2)
+                .cart(cart)
+                .quantity(1)
+                .build();
+        cartItemRepository.save(cartItem2);
+
+        OrderCreateDto orderCreateDto2 = OrderCreateDto.builder()
+                .userId("user1")
+                .postcode("12345")
+                .basicAddress("Basic Address")
+                .detailAddress("Detail Address")
+                .receiverName("John Doe")
+                .receiverPhone("123-456-7890")
+                .message("Message")
+                .build();
+
+        Long orderId2 = orderService.createOrderFromCart(orderCreateDto2);
+
+        //when
+        List<OrderItemDto> orderItemDtos = orderItemRepository.findOrderItemsByOrderId(orderId1);
+        log.info("orderId1: " + orderId1);
+        log.info("orderItemDtos: " + orderItemDtos);
+        List<OrderItemDto> orderItemDtos2 = orderItemRepository.findOrderItemsByOrderId(orderId2);
+        log.info("orderId2: " + orderId2);
+        log.info("orderItemDtos2: " + orderItemDtos2);
+
+        //then
+        Assertions.assertNotNull(orderItemDtos);
+        Assertions.assertNotNull(orderItemDtos2);
+        Assertions.assertEquals(3, orderItemDtos.size());
+        Assertions.assertEquals(2, orderItemDtos2.size());
+
+        // order에서 orderItem 가격 높은순으로 정렬
+        Assertions.assertEquals(item1.getId(), orderItemDtos.get(0).getItemId());
+        Assertions.assertEquals(item2.getId(), orderItemDtos.get(2).getItemId());
+        Assertions.assertEquals(item3.getId(), orderItemDtos.get(1).getItemId());
+        Assertions.assertEquals(item3.getId(), orderItemDtos2.get(0).getItemId());
+        Assertions.assertEquals(item2.getId(), orderItemDtos2.get(1).getItemId());
+    }
+}
