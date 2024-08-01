5e0447b07b2d1f8302de351aa73095734092de9f
RyongJaeKwon <gyj343@gmail.com>
2024-08-01 17:01:29 +0900

    Feat: 주문 조회시 예외 처리 클래스 구현

--- src/main/java/com/kwon/myshop/exception/OrderNotFoundException.java
+++ src/main/java/com/kwon/myshop/exception/OrderNotFoundException.java
@@ -1,2 +1,16 @@
-package com.kwon.myshop.exception;public class OrderNotFoundException {
+package com.kwon.myshop.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class OrderNotFoundException extends MyshopException{
+
+    private static final String MESSAGE = "존재하지 않는 주문입니다.";
+
+    public OrderNotFoundException() {
+        super(MESSAGE);
+    }
+    @Override
+    public HttpStatus getStatusCode() {
+        return HttpStatus.NOT_FOUND;
+    }
 }
