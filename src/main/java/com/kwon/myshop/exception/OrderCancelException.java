2e63e478f95a84d39859ce052f6d1fa90d6acf29
RyongJaeKwon <gyj343@gmail.com>
2024-08-01 17:00:48 +0900

    Feat: 주문 취소 예외 처리 클래스 구현

--- src/main/java/com/kwon/myshop/exception/OrderCancelException.java
+++ src/main/java/com/kwon/myshop/exception/OrderCancelException.java
@@ -1,2 +1,14 @@
-package com.kwon.myshop.exception;public class OrderCancelException {
+package com.kwon.myshop.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class OrderCancelException extends MyshopException {
+
+    public OrderCancelException(String message) {
+        super(message);
+    }
+    @Override
+    public HttpStatus getStatusCode() {
+        return HttpStatus.BAD_REQUEST;
+    }
 }
