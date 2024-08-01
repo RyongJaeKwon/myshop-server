d9846880dc02934386179bd30bb6afe6df757caf
RyongJaeKwon <gyj343@gmail.com>
2024-08-02 02:31:30 +0900

    Fix: findOrderItemsByOrderId 쿼리에 배송 관련 정보 포함하도록 수정

--- src/main/java/com/kwon/myshop/repository/OrderItemRepository.java
+++ src/main/java/com/kwon/myshop/repository/OrderItemRepository.java
@@ -11,7 +11,9 @@ import java.util.List;
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     @Query("select " +
-            "new com.kwon.myshop.dto.OrderItemDto(oi.item.id, oi.item.itemName, oi.item.color, oi.item.size, oi.orderPrice, oi.quantity, ii.fileName) " +
+            "new com.kwon.myshop.dto.OrderItemDto(oi.item.id, oi.item.itemName, oi.item.color, oi.item.size, oi.orderPrice, oi.quantity, ii.fileName, " +
+            "oi.order.delivery.receiverName, oi.order.delivery.receiverPhone, oi.order.delivery.message, " +
+            "oi.order.delivery.address.postcode, oi.order.delivery.address.basic_address, oi.order.delivery.address.detail_address) " +
             "from OrderItem oi " +
             "left join oi.item.imageList ii " +
             "where oi.order.member.userId = :userId and oi.order.id = :orderId and ii.ord = 0 order by oi.orderPrice desc")
