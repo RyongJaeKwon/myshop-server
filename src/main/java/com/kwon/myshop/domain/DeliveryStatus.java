9cd2150a18396fd91a909b6dd0ec66d9cc42124d
RyongJaeKwon <gyj343@gmail.com>
2024-08-01 16:57:42 +0900

    Feat: 배송 상태에 배송중 상태 추가

--- src/main/java/com/kwon/myshop/domain/DeliveryStatus.java
+++ src/main/java/com/kwon/myshop/domain/DeliveryStatus.java
@@ -11,6 +11,7 @@ public enum DeliveryStatus {
 
     DEPOSIT_READY("DEPOSIT_READY", "입금대기"),
     DELIVERY_READY("DELIVERY_READY", "배송준비중"),
+    DELIVERY_IN_PROGRESS("DELIVERY_IN_PROGRESS", "배송중"),
     DELIVERY_COMP("DELIVERY_COMP", "배송완료"),
     DEFAULT("DEFAULT", "-");
 
