0a3e41accf13e8b45d26cef543541d4a81170216
RyongJaeKwon <gyj343@gmail.com>
2024-08-03 03:33:41 +0900

    Feat: OrderController 메서드에 권한 설정 추가

--- src/main/java/com/kwon/myshop/controller/OrderController.java
+++ src/main/java/com/kwon/myshop/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.kwon.myshop.dto.OrderDto;
 import com.kwon.myshop.dto.OrderItemDto;
 import com.kwon.myshop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @PreAuthorize("#orderCreateDto.userId == principal.username")
     @PostMapping("/")
     public Map<String, Long> createOrder(@RequestBody OrderCreateDto orderCreateDto) {
         Long orderId = orderService.createOrder(orderCreateDto);
@@ -24,6 +26,7 @@ public class OrderController {
         return Map.of("RESULT", orderId);
     }
 
+    @PreAuthorize("#orderCreateDto.userId == principal.username")
     @PostMapping("/cart")
     public Map<String, Long> createOrderFromCart(@RequestBody OrderCreateDto orderCreateDto) {
         Long orderId = orderService.createOrderFromCart(orderCreateDto);
@@ -31,6 +34,7 @@ public class OrderController {
         return Map.of("RESULT", orderId);
     }
 
+    @PreAuthorize("#userId == principal.username")
     @GetMapping("/{userId}")
     public List<OrderDto> getOrders(@PathVariable String userId) {
         List<OrderDto> orders = orderService.getOrdersByUserId(userId);
@@ -38,11 +42,13 @@ public class OrderController {
         return orders;
     }
 
+    @PreAuthorize("#userId == principal.username")
     @GetMapping("/{userId}/{orderId}")
     public List<OrderItemDto> getOrderItems(@PathVariable String userId, @PathVariable Long orderId) {
         return orderService.getOrderItems(userId, orderId);
     }
 
+    @PreAuthorize("#userId == principal.username")
     @PostMapping("/{userId}/{orderId}")
     public Map<String, String> cancelOrder(@PathVariable String userId, @PathVariable Long orderId) {
         orderService.cancelOrder(orderId);
